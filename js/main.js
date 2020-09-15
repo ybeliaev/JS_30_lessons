f407295770b8aad0dcb8f2ab2d06aa99437c210a
Jorgen <ybeliaev@gmail.com>
2020-09-15 18:44:23 +0300

    img adding

---
+++ js/main.js
@@ -0,0 +1,40 @@
+console.log("Начало тренировки..");
+
+const TIME_ONE_PUSHUP = 200;
+const TIME_ONE_SQUAT = 400;
+
+const pushUp = (count) => {
+  return new Promise((resolve, reject) => {
+    if (count > 100) {
+      reject(new Error("Приседаний нужно не более 100"));
+    }
+    setTimeout(() => {
+      // вызов колбека в then:
+      // также передача count с resolve позволит использовать это
+      // значение в then в виде value
+      resolve(count);
+    }, count * TIME_ONE_PUSHUP);
+  });
+};
+const squats = (count) => {
+  return new Promise((resolve, reject) => {
+    if (count > 50) {
+      reject(new Error("Слишком много отжиманий. Нужно не более 50"));
+    }
+    setTimeout(() => {
+      resolve(count);
+    }, count * TIME_ONE_SQUAT);
+  });
+};
+
+pushUp(5)
+  .then((value) => {
+    console.log(`Отжался ${value} раз`);
+    return squats(10);
+  })
+  .then((value) => {
+    console.log(`Присел ${value} раз`);
+  })
+  .catch((error) => {
+    console.error("Что-то больше не могу...", error.toString());
+  });
